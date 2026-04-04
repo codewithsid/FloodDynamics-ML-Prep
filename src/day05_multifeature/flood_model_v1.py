a77fcd63eb0e322d5a4ffe6b1957cb12bb7f6cbb
Siddhant Bhardwaj <sid@bjkul.com>
2026-04-04 10:37:53 -0700

    day05: multifeature classification complete

---
+++ src/day05_multifeature/flood_model_v1.py
@@ -0,0 +1,58 @@
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+import numpy as np
+
+data = {
+    "rainfall": [5, 15, 25, 35, 45, 60, 70, 80],
+    "temperature": [5, 15, 25, 35, 45, 60, 70, 80],
+    "soil_moisture": [0.1, 0.2, 0.3, 0.5, 0.6, 0.7, 0.8, 0.9],
+    "flood": [0, 0, 0, 1, 1, 1, 1, 1]
+}
+
+df = pd.DataFrame(data)
+
+x = df[["rainfall", "temperature", "soil_moisture"]] #multidimentsional features
+y = df["flood"] #single dimensional output
+#shape of model (8,3) -> 8 rows and 3 columns (features)
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, random_state = 42)
+model = LogisticRegression() #creating the model object
+model.fit(x_train, y_train) #training the model
+
+y_pred = model.predict(x_test) #predicting the ouput from the test set
+y_prob = model.predict_proba(x_test) #predicting the probabilities of outputs from the test set
+accuracy = accuracy_score(y_test, y_pred) #calculating the accuracty from the model predictions using y_test set
+print("Accuracy:", accuracy)
+print("Model Coefficients:", model.coef_) #representation of the automatically assigned weights to features 
+
+#Visualization of only 2 features (rainfall and soil-moisture)
+x_vis = df[["rainfall", "soil_moisture"]]
+y = df["flood"] 
+
+model_vis = LogisticRegression()
+model_vis.fit(x_vis, y) #fitting truncated model
+
+x_min = x_vis["rainfall"].min() - 5
+x_max = x_vis["rainfall"].max() + 5
+y_min = x_vis["soil_moisture"].min() - 0.1
+y_max = x_vis["soil_moisture"].max() + 0.1
+
+xx, yy, = np.meshgrid(
+    np.linspace(x_min, x_max, 100),
+    np.linspace(y_min, y_max, 100)
+)
+
+grid = np.c_[xx.ravel(), yy.ravel()]
+z = model_vis.predict(grid)
+z = z.reshape(xx.shape)
+
+plt.contourf(xx, yy, z, alpha=0.1)
+plt.scatter(x_vis["rainfall"], x_vis["soil_moisture"], c=y) #c = y is color is yellow for 1 and blue for 0
+plt.xlabel("Rainfall")
+plt.ylabel("Soil Moisture")
+plt.title("Flood Classification Boundary")
+plt.savefig("outputs/day05_contourplot.png")
+plt.show()
